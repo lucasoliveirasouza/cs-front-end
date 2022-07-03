4f2eb4f1c53b7ef4e2332ca2f9d8c11df2e72cd2
lucasoliveirasouza <lycasoliveira@gmail.com>
2022-07-03 13:52:15 -0300

    Criar tela de edição #12

---
+++ lib/views/auth/editar_usuario.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class EditarUsuarioView extends StatefulWidget {
+  const EditarUsuarioView({Key? key}) : super(key: key);
+
+  @override
+  _EditarUsuarioViewState createState() => _EditarUsuarioViewState();
+}
+
+class _EditarUsuarioViewState extends State<EditarUsuarioView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
