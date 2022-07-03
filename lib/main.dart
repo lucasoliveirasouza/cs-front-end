ec1c55fb319761c24f9c846bac16e85a16ef10f8
lucasoliveirasouza <lycasoliveira@gmail.com>
2022-07-03 16:48:40 -0300

    Criar ícone do aplicativo #14

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:provider/provider.dart';
 
+
 void main() {
   runApp(MultiProvider(
     providers: [
@@ -31,4 +32,5 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
 
