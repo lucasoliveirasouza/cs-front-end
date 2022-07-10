fcadbec6739e5524cc16fe38fd00f6a99696e41f
lucasoliveirasouza <lycasoliveira@gmail.com>
2022-07-10 09:48:40 -0300

    Criar service de autor #18

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:csbiblio/services/auth_service.dart';
+import 'package:csbiblio/services/autor_service.dart';
 import 'package:csbiblio/services/usuario_service.dart';
 import 'package:csbiblio/views/auth/auth_check.dart';
 import 'package:flutter/material.dart';
@@ -11,6 +12,7 @@ void main() {
     providers: [
       ChangeNotifierProvider(create: (context) => AuthService()),
       ChangeNotifierProvider(create: (context) => UsuarioService()),
+      ChangeNotifierProvider(create: (context) => AutorService()),
     ],
     child: MyApp(),
   ));
