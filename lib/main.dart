9f3e05b19686af0c9bcc05099036d7b55bfe5b6e
lucasoliveirasouza <lycasoliveira@gmail.com>
2022-08-25 20:10:16 -0300

    Criar gênero

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:csbiblio/services/auth_service.dart';
 import 'package:csbiblio/services/autor_service.dart';
+import 'package:csbiblio/services/genero_service.dart';
 import 'package:csbiblio/services/usuario_service.dart';
 import 'package:csbiblio/views/auth/auth_check.dart';
 import 'package:flutter/material.dart';
@@ -15,6 +16,7 @@ void main() {
       ChangeNotifierProvider(create: (context) => UsuarioService()),
       ChangeNotifierProvider(create: (context) => AutorService()),
       ChangeNotifierProvider(create: (context) => EditoraService()),
+      ChangeNotifierProvider(create: (context) => GeneroService()),
     ],
     child: MyApp(),
   ));
