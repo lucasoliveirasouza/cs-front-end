04626d6773d2052d2cc53e576cc84d23c2295c8d
lucasoliveirasouza <lycasoliveira@gmail.com>
2022-08-27 14:01:16 -0300

    Criar Livro

--- lib/views/livro/livro_cadastrar.dart
+++ lib/views/livro/livro_cadastrar.dart
@@ -1,3 +1,4 @@
+import 'package:csbiblio/componentes/form_field_padrao.dart';
 import 'package:flutter/material.dart';
 
 class LivroCadastrarView extends StatefulWidget {
@@ -8,13 +9,53 @@ class LivroCadastrarView extends StatefulWidget {
 }
 
 class _LivroCadastrarViewState extends State<LivroCadastrarView> {
+  final nome = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Cadastrar livro"),
       ),
-      body: Container(),
+      body: Form(
+        key: formKey,
+        child: Container(
+          padding: EdgeInsets.only(right: 20, top: 20, left: 20),
+          child: ListView(
+            children: [
+              FormFieldPadrao(
+                controle: nome,
+                title: "Nome",
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              FormFieldPadrao(
+                controle: nome,
+                title: "Nome",
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              FormFieldPadrao(
+                controle: nome,
+                title: "Nome",
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              FormFieldPadrao(
+                controle: nome,
+                title: "Nome",
+              ),
+              SizedBox(
+                height: 15,
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
