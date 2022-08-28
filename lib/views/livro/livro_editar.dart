2c114ea66d6d30f2d386d3349a5db0986f4aedf4
lucasoliveirasouza <lycasoliveira@gmail.com>
2022-08-28 12:45:51 -0300

    Criar Livro

---
+++ lib/views/livro/livro_editar.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LivroEditarView extends StatefulWidget {
+  const LivroEditarView({Key? key}) : super(key: key);
+
+  @override
+  State<LivroEditarView> createState() => _LivroEditarViewState();
+}
+
+class _LivroEditarViewState extends State<LivroEditarView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
