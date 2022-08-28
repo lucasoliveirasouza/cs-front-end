af7251d1b770d38fd281da60f69b9f8250edaa93
lucasoliveirasouza <lycasoliveira@gmail.com>
2022-08-27 23:05:19 -0300

    Criar Livro

--- lib/views/livro/livro_cadastrar.dart
+++ lib/views/livro/livro_cadastrar.dart
@@ -1,9 +1,3 @@
-import 'package:csbiblio/componentes/dropdown_padrao.dart';
-import 'package:csbiblio/componentes/form_field_padrao.dart';
-import 'package:csbiblio/models/Autor.dart';
-import 'package:csbiblio/models/Editora.dart';
-import 'package:csbiblio/services/autor_service.dart';
-import 'package:csbiblio/services/editora_service.dart';
 import 'package:flutter/material.dart';
 
 class LivroCadastrarView extends StatefulWidget {
@@ -24,81 +18,7 @@ class _LivroCadastrarViewState extends State<LivroCadastrarView> {
       appBar: AppBar(
         title: Text("Cadastrar livro"),
       ),
-      body: Form(
-        key: formKey,
-        child: Container(
-          padding: EdgeInsets.only(right: 20, top: 20, left: 20),
-          child: ListView(
-            children: [
-              FormFieldPadrao(
-                controle: nome,
-                title: "Nome",
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              FormFieldPadrao(
-                controle: nome,
-                title: "Nome da saga",
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              FormFieldPadrao(
-                controle: nome,
-                title: "Edição",
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              FormFieldPadrao(
-                controle: nome,
-                title: "Ano de publicação",
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              FormFieldPadrao(
-                controle: nome,
-                title: "Código",
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              DropdownPadrao<Autor>(
-                nome: "Autor",
-                future: AutorService().getAll(),
-                onSelect: (value) {
-                  autorId = value;
-                },
-                initialValue: autorId,
-                child: 'nome',
-                value: 'id',
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              DropdownPadrao<Editora>(
-                nome: "Autor",
-                future: EditoraService().getAll(),
-                onSelect: (value) {
-                  autorId = value;
-                },
-                initialValue: autorId,
-                child: 'nome',
-                value: 'id',
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              FormFieldPadrao(
-                controle: nome,
-                title: "Quantidade",
-              ),
-            ],
-          ),
-        ),
-      ),
+      body: Container(),
     );
   }
 }
