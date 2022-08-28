1e56758cff6fcf527f0321afc54ffdad7ce9b201
lucasoliveirasouza <lycasoliveira@gmail.com>
2022-08-28 10:31:40 -0300

    Criar Livro

--- lib/models/Livro.dart
+++ lib/models/Livro.dart
@@ -4,64 +4,68 @@ import 'Genero.dart';
 
 class Livro {
   Livro({
-    required this.id,
-    required this.nomeSaga,
-    required this.titulo,
-    required this.edicao,
-    required this.anoPublicacao,
-    required this.codigo,
-    required this.quantidade,
-    required this.autor,
-    required this.editora,
-    required this.genero,
-  });
+    int? id,
+    String? nomeSaga,
+    String? titulo,
+    String? edicao,
+    int? anoPublicacao,
+    String? codigo,
+    int? quantidade,
+    Autor? autor,
+    Editora? editora,
+    List<Genero>? genero,
+  }) {
+    _id = id;
+    _nomeSaga = nomeSaga;
+    _titulo = titulo;
+  }
 
   Livro.fromJson(dynamic json) {
-    id = json['id'];
-    nomeSaga = json['nomeSaga'];
-    titulo = json['titulo'];
-    edicao = json['edicao'];
-    anoPublicacao = json['anoPublicacao'];
-    codigo = json['codigo'];
-    quantidade = json['quantidade'];
-    autor = (json['autor'] != null ? Autor.fromJson(json['autor']) : null)!;
-    editora =
+    _id = json['id'];
+    _nomeSaga = json['nomeSaga'];
+    _titulo = json['titulo'];
+    _edicao = json['edicao'];
+    _anoPublicacao = json['anoPublicacao'];
+    _codigo = json['codigo'];
+    _quantidade = json['quantidade'];
+    _autor = (json['autor'] != null ? Autor.fromJson(json['autor']) : null)!;
+    _editora =
         (json['editora'] != null ? Editora.fromJson(json['editora']) : null)!;
     if (json['genero'] != null) {
-      genero = [];
+      _genero = [];
       json['genero'].forEach((v) {
-        genero?.add(Genero.fromJson(v));
+        _genero?.add(Genero.fromJson(v));
       });
     }
   }
-  int? id;
-  String? nomeSaga;
-  String? titulo;
-  String? edicao;
-  int? anoPublicacao;
-  String? codigo;
-  int? quantidade;
-  Autor? autor;
-  Editora? editora;
-  List<Genero>? genero;
+  int? _id;
+  String? _nomeSaga;
+  String? _titulo;
+  String? _edicao;
+  int? _anoPublicacao;
+  String? _codigo;
+  int? _quantidade;
+  Autor? _autor;
+  Editora? _editora;
+  List<Genero>? _genero;
 
   Map<String, dynamic> toJson() {
     final map = <String, dynamic>{};
-    map['id'] = id;
-    map['nomeSaga'] = nomeSaga;
-    map['titulo'] = titulo;
-    map['edicao'] = edicao;
-    map['anoPublicacao'] = anoPublicacao;
-    map['codigo'] = codigo;
-    map['quantidade'] = quantidade;
-    if (autor != null) {
-      map['autor'] = autor!.toJson();
+    map['id'] = _id;
+    map['nomeSaga'] = _nomeSaga;
+    map['titulo'] = _titulo;
+    map['edicao'] = _edicao;
+    map['anoPublicacao'] = _anoPublicacao;
+    map['codigo'] = _codigo;
+    map['quantidade'] = _quantidade;
+    if (_autor != null) {
+      map['autor'] = _autor!.toJson();
     }
-    if (editora != null) {
-      map['editora'] = editora!.toJson();
+    if (_editora != null) {
+      map['editora'] = _editora!.toJson();
     }
-    if (genero != null) {
-      map['genero'] = genero!.map((v) => v.toJson()).toList();
+    if (_genero != null) {
+      map['genero'] = _genero!.map((v) => v.toJson()).toList();
     }
     return map;
   }
